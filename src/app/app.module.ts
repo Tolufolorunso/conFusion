354f7c3fecfd4c5733e7d2fdc44a50f5d2d6b047
Tolulope Folorunso <tolufolorunso@yahoo.com>
2019-09-17 12:32:11 +0100

    menu updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HttpClientModule } from "@angular/common/http";
 import { MatRippleModule } from "@angular/material";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 
 import "hammerjs";
 import { AppComponent } from "./app.component";
@@ -23,7 +26,10 @@ import { MenuComponent } from "./menu/menu.component";
     HttpClientModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
